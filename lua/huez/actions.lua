1c7e4e1fca046bf5bca3639dd68d422a43bc2f75
vague2k <ilovedrawing056@gmail.com>
2024-01-24 17:37:55 -0500

    initial

---
+++ lua/huez/actions.lua
@@ -0,0 +1,44 @@
+local telescope_actions = require("telescope.actions")
+local telescope_actions_state = require("telescope.actions.state")
+local default_conf = require("huez.config").default_config
+local persist = require("huez.persist")
+
+local actions = {}
+
+---Preview color scheme when going to the next selection
+---@param bufnr buffer
+actions.next_color = function(bufnr)
+	telescope_actions.move_selection_next(bufnr)
+	local selected = telescope_actions_state.get_selected_entry()
+	local cmd = "colorscheme " .. selected[1]
+	vim.cmd(cmd)
+end
+
+---Preview color scheme when going to the previous selection
+---@param bufnr buffer
+actions.prev_color = function(bufnr)
+	telescope_actions.move_selection_previous(bufnr)
+	local selected = telescope_actions_state.get_selected_entry()
+	local cmd = "colorscheme " .. selected[1]
+	vim.cmd(cmd)
+end
+
+actions.get_colorscheme = function()
+	local ok, colorscheme = pcall(__, default_conf.file_path)
+	if ok then
+		return colorscheme
+	else
+		return default_conf.fallback
+	end
+end
+
+---@param colorscheme string
+actions.save_colorscheme = function(colorscheme)
+	vim.cmd("hi clear")
+	vim.cmd("syntax reset")
+	vim.cmd("colorscheme " .. colorscheme)
+	persist.check_if_file_exists()
+	persist._write_theme_to_file(colorscheme)
+end
+
+return actions
---
+++ lua/huez/persist.lua
@@ -0,0 +1,36 @@
+local default_conf = require("huez.config").default_config
+
+local persist = {}
+
+---checks if file_path from config exists. If it does exists, It will do nothing with it and return true
+--
+---If it does NOT exist, it will return false and create it
+persist.check_if_file_exists = function()
+	local file = io.open(default_conf.file_path, "r")
+	if file then
+		io.close(file)
+		return
+	else
+		local new_file = io.open(default_conf.file_path, "w")
+		if new_file then
+			io.close(new_file)
+			return
+		end
+	end
+end
+
+---@param colorscheme string
+persist._write_theme_to_file = function(colorscheme)
+	vim.loop.fs_open(default_conf.file_path, "w", 432, function(_, fd)
+		if fd ~= nil then
+			local string_to_write = '"' .. colorscheme .. '"'
+			vim.loop.fs_write(fd, string_to_write, nil, function()
+				vim.loop.fs_close(fd)
+			end)
+		else
+			print(_)
+		end
+	end)
+end
+
+return persist

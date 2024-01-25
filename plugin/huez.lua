3c1f198251512e771a6e51ef309d641b5a53230d
vague2k <121782036+vague2k@users.noreply.github.com>
2024-01-25 00:17:15 -0500

    Create huez.lua

---
+++ plugin/huez.lua
@@ -0,0 +1,10 @@
+if vim.fn.has("nvim-0.7.0") == 0 then
+  vim.api.nvim_err_writeln("Huez requires at least nvim-0.7.0.1")
+  return
+end
+
+-- make sure this file is loaded only once
+if vim.g.loaded_huez == 1 then
+  return
+end
+vim.g.loaded_huez = 1

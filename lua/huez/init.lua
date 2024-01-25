03e5722f1fa5195fe35b9f91da806fdb115fdbc0
vague2k <121782036+vague2k@users.noreply.github.com>
2024-01-25 00:16:48 -0500

    add: file check in setup, user cmd

--- lua/huez/init.lua
+++ lua/huez/init.lua
@@ -6,6 +6,7 @@ local action_state = require("telescope.actions.state")
 
 local huez_actions = require("huez.actions")
 local huez_conf = require("huez.config")
+local huez_persist = require("huez.persist")
 
 local M = {}
 
@@ -24,24 +25,27 @@ local function filter_default_themes()
   return default_omitted
 end
 
-M.huez = function(opts)
+-- TODO: reset colorscheme if picker is closed without selection
+M.pick_colorscheme = function(opts)
   opts = opts or {}
-
   pickers
     .new(huez_conf.default_config.picker_opts, {
-      prompt_title = "huez",
+      prompt_title = "Huez",
       finder = finders.new_table(filter_default_themes()),
       sorter = conf.generic_sorter(opts),
       attach_mappings = function(bufnr, map)
         actions.select_default:replace(function()
           actions.close(bufnr)
+
           local selection = action_state.get_selected_entry()
           local colorscheme
+
           if selection == nil then
             vim.notify("Huez: selection isn't valid")
           else
             colorscheme = selection[1]
           end
+
           huez_actions.save_colorscheme(colorscheme)
         end)
 
@@ -49,6 +53,8 @@ M.huez = function(opts)
         map("i", "<C-k>", huez_actions.prev_color)
         map("i", "<C-n>", huez_actions.next_color)
         map("i", "<C-p>", huez_actions.prev_color)
+        map("i", "<Up>", huez_actions.prev_color)
+        map("i", "<Down>", huez_actions.next_color)
 
         map("n", "j", huez_actions.next_color)
         map("n", "k", huez_actions.prev_color)
@@ -63,9 +69,12 @@ end
 
 M.setup = function(opts)
   opts = opts or {}
+  huez_persist.check_if_file_exists()
   for k, v in pairs(opts) do
     M[k] = v
   end
+
+  vim.api.nvim_create_user_command("Huez", M.pick_colorscheme, {})
 end
 
 return M

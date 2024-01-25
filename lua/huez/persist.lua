9b38dd695c9860ebb7953f5a041ee91744bb3b08
vague2k <121782036+vague2k@users.noreply.github.com>
2024-01-24 22:56:09 -0500

    add: docstring, change: nowrap param in str quotes

--- lua/huez/persist.lua
+++ lua/huez/persist.lua
@@ -2,9 +2,8 @@ local default_conf = require("huez.config").default_config
 
 local persist = {}
 
----checks if file_path from config exists. If it does exists, It will do nothing with it and return true
---
----If it does NOT exist, it will return false and create it
+--checks if file_path from config exists. If it does exists, It will do nothing with it and return
+--If it does NOT exist, create it
 persist.check_if_file_exists = function()
   local file = io.open(default_conf.file_path, "r")
   if file then
@@ -23,7 +22,7 @@ end
 persist._write_theme_to_file = function(colorscheme)
   vim.loop.fs_open(default_conf.file_path, "w", 432, function(_, fd)
     if fd ~= nil then
-      local string_to_write = '"' .. colorscheme .. '"'
+      local string_to_write = colorscheme
       vim.loop.fs_write(fd, string_to_write, nil, function()
         vim.loop.fs_close(fd)
       end)

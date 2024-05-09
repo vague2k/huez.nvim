b12571cb6e2c2d36c2669646b0435d4fa0a2a2d2
albert <ilovedrawing056@gmail.com>
2024-05-09 08:27:42 -0400

    fix(ci): colorscheme_spec and formate_tests.yml

--- test/huez/colorscheme_api_spec.lua
+++ test/huez/colorscheme_api_spec.lua
@@ -1,22 +1,27 @@
 local c = require("huez.api").colorscheme
 
 local eq = assert.are.same
+local path = vim.fs.normalize("/tmp/huez-theme-test")
 
 describe("HUEZ: colorscheme api", function()
   it("can read and write to a huez-theme file", function()
-    local mock_path = vim.fs.normalize("~/.cache/nvim/huez-theme-test")
-
-    local written = c.save("test", mock_path)
-    local read = c.get(mock_path)
+    local written = c.save("test", path)
+    local read = c.get(path)
 
     eq(not nil, written)
     eq("test", read)
 
-    os.remove(mock_path)
+    os.remove(path)
   end)
 
   it("can return default installed themes", function()
     local themes = c.installed({})
+
+    -- in workflows we also test on neovim nightly, this if statement handles that case
+    if vim.fn.has("nvim-0.10") == 1 then
+      eq(26, #themes)
+      return
+    end
 
     eq(21, #themes)
   end)

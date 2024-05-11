72f1cedb529ed76901d3eb496702f6065bdc5841
vague2k <ilovedrawing056@gmail.com>
2024-05-11 15:22:41 -0400

    refactor(live action): use global sliding window, add FIXME comment

--- lua/huez/pickers/live/actions.lua
+++ lua/huez/pickers/live/actions.lua
@@ -22,6 +22,7 @@ M.get_pkgs = function()
 end
 
 local pkgs = M.get_pkgs()
+local global_window = {}
 
 M.optimistic_preview_next = function(bufnr)
   telescope_actions.move_selection_next(bufnr)
@@ -30,21 +31,29 @@ M.optimistic_preview_next = function(bufnr)
   local picker = state.get_current_picker(bufnr)
   local row = picker:get_selection_row()
 
-  -- the results picker already has a cursor on the 1st index, so when the selection moves, we get index == 2
+  -- the results list selects index 1 by default, so when the selection moves, we get index == 2
   -- thus, we have to substract 1 from the current index
   local index = picker:get_index(row) - 1
+  local window_size = index + 4
+  -- reinit the global window to make we only keep track of 5 values at a time
+  -- We need this global window so when we go to the previous selection we can slide the window back accordingly
+  -- FIXME: how can we shift the window back when going to prev selection using the global window?
+  global_window = {}
+  for i = index, window_size do
+    -- if we go past #pkgs go back to the beginning of the results list
+    if i == 0 then
+      return
+    end
 
-  local window = {}
-  for i = index, index + 4 do
+    -- near the end of the list, if the index extends past #pkgs range, shift the list down.
     if i > #pkgs then
-      table.remove(window, 1)
-      vim.print(window)
+      table.remove(global_window, 1)
       break
     end
-    -- FIXME: errors when going past #pkgs in the results window
-    table.insert(window, pkgs[i].pkgname)
+
+    table.insert(global_window, pkgs[i].pkgname)
   end
-  vim.print(window)
+  vim.print(global_window)
 end
 
 M.optimistic_preview_prev = function(bufnr)
--- lua/huez/pickers/live/mappings.lua
+++ lua/huez/pickers/live/mappings.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.attach = function(attach_map, actions)
   attach_map("n", "j", actions.optimistic_preview_next)
+  attach_map("n", "k", actions.optimistic_preview_prev)
 end
 
 return M

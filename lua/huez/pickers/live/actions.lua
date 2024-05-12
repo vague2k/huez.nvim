2dc4beb94bc428533c29dd316f5f5508c38c999a
vague2k <ilovedrawing056@gmail.com>
2024-05-11 22:07:30 -0400

    i hate this

--- lua/huez/pickers/live/actions.lua
+++ lua/huez/pickers/live/actions.lua
@@ -29,26 +29,13 @@ M.optimistic_preview_next = function(bufnr)
 
   ---@type Picker
   local picker = state.get_current_picker(bufnr)
-  local row = picker:get_selection_row()
+  local index = picker:get_selection_row()
 
-  -- the results list selects index 1 by default, so when the selection moves, we get index == 2
-  -- thus, we have to substract 1 from the current index
-  local index = picker:get_index(row) - 1
-  local window_size = index + 4
-  -- reinit the global window to make we only keep track of 5 values at a time
-  -- We need this global window so when we go to the previous selection we can slide the window back accordingly
-  -- FIXME: how can we shift the window back when going to prev selection using the global window?
   global_window = {}
-  for i = index, window_size do
-    -- if we go past #pkgs go back to the beginning of the results list
-    if i == 0 then
+  for i = index, index + 4 do
+    -- if we reach the end of the array, starting from the beg. there is nothing else to shift
+    if i == 0 or i > #pkgs then
       return
-    end
-
-    -- near the end of the list, if the index extends past #pkgs range, shift the list down.
-    if i > #pkgs then
-      table.remove(global_window, 1)
-      break
     end
 
     table.insert(global_window, pkgs[i].pkgname)
@@ -58,6 +45,49 @@ end
 
 M.optimistic_preview_prev = function(bufnr)
   telescope_actions.move_selection_previous(bufnr)
+  local picker = state.get_current_picker(bufnr)
+  local index = picker:get_selection_row() + 1
+
+  -- FIXME: my head fucking hurts and i give up
+
+  --[[ -- vim.print(index)
+  -- if we reach the beg. of the array, starting from the end there is nothing else to shift
+  if index == 1 then
+    return
+  end
+
+  -- if a user immediately starts from the end...
+  if index == #pkgs and #global_window <= 0 then
+    vim.print("STARTING FROM END")
+    for i = index, index - 4, -1 do
+      -- global_window = {}
+      table.remove(global_window, i)
+      table.insert(global_window, pkgs[i].pkgname)
+    end
+    vim.print(global_window)
+    return
+  end
+
+  -- if window slides backwards, after moving forwards at least once,
+  if #pkgs - 4 == index and #global_window > 4 then
+    vim.print(" MORE THAN 4")
+    table.remove(global_window, #global_window)
+    table.insert(global_window, 1, pkgs[index].pkgname)
+    vim.print(global_window)
+    return
+  end
+
+  -- -- if a user immediately starts from the end...
+  -- if #global_window <= 0 then
+  --   for i = index, index - 4, -1 do
+  --     -- vim.print(pkgs[i])
+  --     global_window = {}
+  --     table.insert(global_window, pkgs[i].pkgname)
+  --   end
+  --   return
+  -- end
+
+  vim.print(global_window) ]]
 end
 
 return M

37a35ac2892eeca069e1e72cf8c6cdeecad19a88
vague2k <ilovedrawing056@gmail.com>
2024-06-06 00:38:55 -0400

    fix(live): `suppress_messages` conf opt didn't apply to `:HuezLive`

--- lua/huez-manager/utils/log.lua
+++ lua/huez-manager/utils/log.lua
@@ -12,4 +12,17 @@ M.notify = function(msg, level)
   return vim.notify(msg, vim.log.levels[level:upper()], { title = "Huez.nvim" })
 end
 
+--- util wrapper for vim.notify_once
+---@param msg any
+---@param level string
+---@param opts? table
+---@return nil
+M.notify_once = function(msg, level, opts)
+  opts = vim.tbl_deep_extend("force", { title = "Huez.nvim" }, opts or {})
+  if config.current.suppress_messages == true then
+    return
+  end
+  return vim.notify_once(msg, vim.log.levels[level:upper()], opts)
+end
+
 return M

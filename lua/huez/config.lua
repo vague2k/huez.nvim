e20b833b5c90c8ef4db7fc3b3976626b23a254a6
cool-pants <muban.sg@gmail.com>
2024-07-07 12:20:15 +0530

    feat: add config loader
    Added a set_theme function to act as the setter instead of the original
    vim.cmd.colorscheme. The setters are loaded on load of plugin using the
    load_theme_configs

--- lua/huez/config.lua
+++ lua/huez/config.lua
@@ -14,8 +14,15 @@ local M = {}
 ---@field path? string
 ---@field fallback? string
 ---@field suppress_messages? boolean
+---@field theme_config_dir? string
 ---@field exclude? string[]
 ---@field picker? Huez.Config.Pickers
+
+---@alias ThemeSetter fun(theme: string): boolean
+
+---@class Huez.ThemeConfig
+---@field styles string[] -- this denotes the styles like moon, night, day, etc.
+---@field set_theme ThemeSetter
 
 ---@class Huez.InternalConfig
 -- TODO: write a config validator
@@ -26,6 +33,8 @@ local DEFAULT_SETTINGS = {
   fallback = "default",
   ---@type boolean
   suppress_messages = true,
+  ---@type string
+  theme_config_dir = nil,
   ---@type string[]
   exclude = {
     "desert",
@@ -88,6 +97,59 @@ M.set = function(user_opts)
   M.current = vim.tbl_deep_extend("force", vim.deepcopy(M.current), user_opts)
 end
 
+---@type table<string, ThemeSetter>
+M.theme_setters = {}
+
+-- Will load all the theme configs into memory.
+M.load_theme_configs = function()
+  local theme_config_dir = M.current.theme_config_dir
+
+  -- check if config dir is non null and exists
+  -- if it does not, then just return
+  if theme_config_dir == nil or vim.fn.isdirectory(theme_config_dir) == 0 then
+    return
+  end
+
+  local theme_manager = require("huez-manager.api")
+
+  -- if it does exist, load them into memory
+  for _, theme in pairs(theme_manager.colorscheme.installed(M.current.exclude)) do
+    ---@type boolean
+    local ok
+    ---@type Huez.ThemeConfig
+    local conf
+
+    ok, conf = pcall(require, theme_config_dir .. "/" .. theme)
+    -- if cannot load config, means no config found => continue
+    if not ok then
+      goto continue
+    end
+
+    for _, theme_style in pairs(conf.styles) do
+      M.theme_setters[theme .. "-" .. theme_style] = conf.set_theme
+    end
+
+    ::continue::
+  end
+end
+
+-- Use this to set the theme instead of manually setting
+---@param theme? string
+---@return boolean
+M.set_theme = function(theme)
+  if theme == nil then
+    return false
+  end
+
+  if M.theme_setters[theme] ~= nil then
+    return M.theme_setters[theme](theme)
+  end
+
+  local ok, _ = pcall(vim.cmd.colorscheme, theme)
+
+  return ok
+end
+
 --- setup the specific files according to path,
 -- currently not supporting windows cuz I'm not tryna deal with it lol
 -- someone will make a PR eventually
@@ -111,24 +173,24 @@ M.handle_theme_on_setup = function()
 
     -- if the theme does not exist, resort to fallback, and write fallback to file
     if theme_name then
-      local ok, _ = pcall(vim.cmd.colorscheme, theme_name)
+      local ok = M.set_theme(theme_name)
 
       if not ok then
         file = io.open(huez_file, "w")
 
         if file then
-          ok, _ = pcall(vim.cmd.colorscheme, fallback)
+          ok = M.set_theme(fallback)
 
           if not ok then
             file:write("default")
-            vim.cmd.colorscheme("default")
+            M.set_theme("default")
             vim.notify("The theme " .. theme_name .. " does not exist, fell back to " .. fallback, 3, notify_opts)
             vim.notify("The theme " .. fallback .. " does not exist, fell back to nvim's default theme", 3, notify_opts)
             return
           end
 
           file:write(fallback)
-          vim.cmd.colorscheme(fallback)
+          M.set_theme(fallback)
           vim.notify("The theme " .. theme_name .. " does not exist, fell back to " .. fallback, 3, notify_opts)
         end
         return

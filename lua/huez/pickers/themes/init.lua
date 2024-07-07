301eb2b35a67146ff1c590e2107457f390d93d64
vague2k <ilovedrawing056@gmail.com>
2024-07-07 15:25:40 -0400

    Set the current color scheme as a preselection in the picker
    this was a qol feature of pre 1.0, but forgot to include it in the
    rewrite

--- lua/huez/pickers/themes/init.lua
+++ lua/huez/pickers/themes/init.lua
@@ -5,8 +5,18 @@ local picker_utils = require("huez.pickers.utils")
 local mappings = require("huez.pickers.themes.mappings")
 local api = require("huez-manager.api")
 
-local function render()
+local function render(opts)
   local themes = api.colorscheme.installed()
+
+  local current_scheme = vim.g.colors_name or "default"
+  local default_index = 1
+  for i, theme in ipairs(themes) do
+    if theme == current_scheme then
+      default_index = i
+      break
+    end
+  end
+  opts.default_selection_index = default_index
 
   picker_utils.picker_builder({
     picker = "themes",
@@ -20,7 +30,7 @@ local function render()
     mappings = function(map)
       mappings.attach(map, actions)
     end,
-  })
+  }, opts)
 end
 
 return {

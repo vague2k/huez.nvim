8f96ddf5a2a2fd0b2178b28215c2d45bf98ac6e7
Luke Mattfeld <lukemattfeld@gmail.com>
2024-07-08 10:11:55 -0700

    fixed formatting

--- lua/huez/pickers/themes/init.lua
+++ lua/huez/pickers/themes/init.lua
@@ -6,34 +6,34 @@ local mappings = require("huez.pickers.themes.mappings")
 local api = require("huez-manager.api")
 
 local function render(opts)
-    opts = opts or {}
-    local themes = api.colorscheme.installed()
+  opts = opts or {}
+  local themes = api.colorscheme.installed()
 
-    local current_scheme = vim.g.colors_name or "default"
-    local default_index = 1
-    for i, theme in ipairs(themes) do
-        if theme == current_scheme then
-            default_index = i
-            break
-        end
+  local current_scheme = vim.g.colors_name or "default"
+  local default_index = 1
+  for i, theme in ipairs(themes) do
+    if theme == current_scheme then
+      default_index = i
+      break
     end
-    opts.default_selection_index = default_index
+  end
+  opts.default_selection_index = default_index
 
-    picker_utils.picker_builder({
-        picker = "themes",
-        prompt_title = "Huez",
-        finder = {
-            results = themes,
-        },
-        default_action = function()
-            actions.save_on_select(telescope_action_state)
-        end,
-        mappings = function(map)
-            mappings.attach(map, actions)
-        end,
-    }, opts)
+  picker_utils.picker_builder({
+    picker = "themes",
+    prompt_title = "Huez",
+    finder = {
+      results = themes,
+    },
+    default_action = function()
+      actions.save_on_select(telescope_action_state)
+    end,
+    mappings = function(map)
+      mappings.attach(map, actions)
+    end,
+  }, opts)
 end
 
 return {
-    render = render,
+  render = render,
 }
